8b06853e6b4be564845f54cdd7b0b92a1a0deb1d
henry <www.henrymarstin123@gmail.com>
2022-05-19 23:34:08 +0100

    Added rainbow mode, fade mode and fixed some bugs

--- etch.js
+++ etch.js
@@ -5,7 +5,10 @@ const fade = document.getElementById("fadeButton");
 const fadeOn = document.getElementById("on");
 const fadeOff = document.getElementById("off");
 
-/*
+const onRain = document.getElementById("onRain");
+const offRain = document.getElementById("offRain");
+
+
 const randomColor = () => {
     let r = Math.floor(Math.random() * (255 - 0 + 1) + 0)
     let g = Math.floor(Math.random() * (255 - 0 + 1) + 0)
@@ -13,7 +16,7 @@ const randomColor = () => {
     let color = `rgb(${r}, ${g}, ${b})`
     return color;
 }
-*/
+
 function makeRows(rows, cols) {
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
@@ -21,19 +24,35 @@ function makeRows(rows, cols) {
     cell.style.width = 560 / cols - 2 + "px";
     container.appendChild(cell).className = "grid-item";
     cell.addEventListener('mouseover', function() {
+      if (onRain.style.backgroundColor == "lime") {
+        cell.style.backgroundColor = randomColor();
+      } else if (offRain.style.backgroundColor == "lime"){
         cell.style.backgroundColor = changeColor();
+      } else {
+        cell.style.backgroundColor = changeColor();
+      }
         cell.style.opacity = parseFloat(cell.style.opacity) + 0.10;
     });
-    fadeOn.addEventListener("click", function() {
-      cell.style.opacity = 0.1;
-      fadeOff.style.backgroundColor = "white";
-      fadeOn.style.backgroundColor = "lime";
-    })
-    fadeOff.addEventListener("click", function() {
-      cell.style.opacity = 1;
-      fadeOn.style.backgroundColor = "white";
-      fadeOff.style.backgroundColor = "lime";
-    })
+        // Fade Mode
+        fadeOn.addEventListener("click", function() {
+          cell.style.opacity = 0.1;
+          fadeOff.style.backgroundColor = "white";
+          fadeOn.style.backgroundColor = "lime";
+        })
+        fadeOff.addEventListener("click", function() {
+          cell.style.opacity = 1;
+          fadeOn.style.backgroundColor = "white";
+          fadeOff.style.backgroundColor = "lime";
+        })
+        // Rainbow Mode
+        onRain.addEventListener("click", function() {
+          offRain.style.backgroundColor = "white";
+          onRain.style.backgroundColor = "lime";
+        })
+        offRain.addEventListener("click", function() {
+          onRain.style.backgroundColor = "white";
+          offRain.style.backgroundColor = "lime";
+        })
   };
 };
 
